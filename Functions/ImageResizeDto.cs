0a4003d23a8c16b7adc0f2fc23107bd4454ab10a
SMARAPD-Ederson Maschio <etmaschio@smarapd.com.br>
2022-04-11 08:48:23 -0300

    Include functions

---
+++ Functions/ImageResizeDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Functions
+{
+    public class ImageResizeDto
+    {
+         public string FileName { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Url { get; set; }
+        public string ImageContainer { get; set; }
+    }
+}
